e60d16a37d8e9cfab0c2cbe2a06267754465023a
JamesZhao2022 <97421992+JamesZhao2022@users.noreply.github.com>
2025-06-05 20:06:29 +0800

    feat: Fix color display issue
    feat: Fix color display issue

--- lib/src/generator.dart
+++ lib/src/generator.dart
@@ -69,11 +69,10 @@ class CodeGenerator {
 
       if (path.fill != null) {
         // Replace fill with dynamic color
-        buffer.write(
-            ' fill="\' + getColor($i, color, colors, \'${path.fill}\') + \'"');
+        buffer
+            .write(' fill="\${getColor($i, color, colors, \'${path.fill}\')}"');
       } else {
-        buffer.write(
-            ' fill="\' + getColor($i, color, colors, \'#333333\') + \'"');
+        buffer.write(' fill="\${getColor($i, color, colors, \'#333333\')}"');
       }
 
       // Add other attributes
